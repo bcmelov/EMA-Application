02906a49de9bceb2834aed84c2c7d586be94b1bb
cmelova.bara <cmelova.b@gmail.com>
2021-03-25 09:00:05 +0100

    user list MockData completed

--- app/src/main/java/com/example/emaapp/view/UserListFragment.kt
+++ app/src/main/java/com/example/emaapp/view/UserListFragment.kt
@@ -5,12 +5,14 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.emaapp.R
 import com.example.emaapp.data.DataSource
+import com.example.emaapp.data.User
 
 //fragment with display of list of the attendees as RecyclerViewer
 class UserListFragment : Fragment(R.layout.fragment_user_list) {
@@ -28,6 +30,7 @@ class UserListFragment : Fragment(R.layout.fragment_user_list) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+        hwStars()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,14 +38,23 @@ class UserListFragment : Fragment(R.layout.fragment_user_list) {
         adapter = Adapter(DataSource.users)
         val rv = view.findViewById<RecyclerView>(R.id.recyclerView)
         rv.adapter = adapter
-        val username = arguments?.getString(KEY_USERNAME)
-        view.findViewById<TextView>(R.id.user_name)
+//        val username = arguments?.getString(KEY_USERNAME)
     }
+
+        private fun hwStars() {
+            view?.findViewById<ImageView>(R.id.list_hw1)?.setState1(User.BaraCmelova.push1)
+            view?.findViewById<ImageView>(R.id.list_hw2)?.setState2(User.BaraCmelova.push2)
+            view?.findViewById<ImageView>(R.id.list_hw3)?.setState3(User.BaraCmelova.push3)
+            view?.findViewById<ImageView>(R.id.list_hw4)?.setState4(User.BaraCmelova.push4)
+            view?.findViewById<ImageView>(R.id.list_hw5)?.setState5(User.BaraCmelova.push5)
+            view?.findViewById<ImageView>(R.id.list_hw6)?.setState6(User.BaraCmelova.push6)
+        }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.users_filter, menu)
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         adapter.users = when (item.itemId) {
@@ -53,4 +65,25 @@ class UserListFragment : Fragment(R.layout.fragment_user_list) {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun ImageView.setState1 (done : Boolean) {
+        val id = if (done) setImageResource(R.drawable.ic_one_done) else setImageResource(R.drawable.ic_one)
+    }
+    private fun ImageView.setState2 (done : Boolean) {
+        val id= if (done) setImageResource(R.drawable.ic_two_done) else setImageResource(R.drawable.ic_two)
+    }
+    private fun ImageView.setState3 (done : Boolean) {
+        val id= if (done) setImageResource(R.drawable.ic_three_done) else setImageResource(R.drawable.ic_three)
+    }
+    private fun ImageView.setState4 (done : Boolean) {
+        val id= if (done) setImageResource(R.drawable.ic_four_done) else setImageResource(R.drawable.ic_four)
+    }
+    private fun ImageView.setState5 (done : Boolean) {
+        val id= if (done) setImageResource(R.drawable.ic_five_done) else setImageResource(R.drawable.ic_five)
+    }
+    private fun ImageView.setState6 (done : Boolean) {
+        val id= if (done) setImageResource(R.drawable.ic_six_done) else setImageResource(R.drawable.ic_six)
+    }
 }
+
+
